6c1e876395b19f5b73e7bb9b14e7e8279304e5c2
NazarioLuis <nazarioluisaf@gmail.com>
2026-01-08 11:36:46 -0300

    update: set version to 1.0.0 in pom.xml and extend Pinv01267WsApplication for servlet support

--- src/main/java/py/edu/facitec/pinv01_267/pinv01_267_ws/Pinv01267WsApplication.java
+++ src/main/java/py/edu/facitec/pinv01_267/pinv01_267_ws/Pinv01267WsApplication.java
@@ -2,13 +2,20 @@ package py.edu.facitec.pinv01_267.pinv01_267_ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Pinv01267WsApplication {
+public class Pinv01267WsApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Pinv01267WsApplication.class, args);
 		System.out.println(">>>> RUN");
 	}
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Pinv01267WsApplication.class);
+    }
 
 }
